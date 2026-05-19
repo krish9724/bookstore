22aa8ea190330276288cdb827773c6493fc617c7
krish9724 <149668631+krish9724@users.noreply.github.com>
2026-05-19 20:44:04 +0530

    Update navbar.jsx

--- Fronted/src/components/navbar.jsx
+++ Fronted/src/components/navbar.jsx
@@ -56,7 +56,7 @@ function Navbar() {
   return (
     <>
       <div
-        className={` max-w-screen-2xl container mx-auto md:px-20 px-4 dark:bg-slate-900 dark:text-white fixed top-0 left-0 right-0 z-50 ${
+        className={` max-w-screen-2xl container mx-auto overflow-y-hidden md:px-20 px-4 dark:bg-slate-900 dark:text-white fixed top-0 left-0 right-0 z-50 ${
           sticky
             ? "sticky-navbar shadow-md bg-base-200 dark:bg-slate-600 dark:text-white duration-300 transition-all ease-in-out"
             : ""
